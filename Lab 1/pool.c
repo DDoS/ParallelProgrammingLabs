6c6f4a419a9fb99a306dd3a14630ae4ca0a12d08
Aleksi Sapon <aleksi@sapon.ca>
2016-10-08 18:06:17 -0400

    Finish the makefile and stub out pool.c and convolve.c

---
+++ Lab 1/pool.c
@@ -0,0 +1,5 @@
+#include <omp.h>
+
+void transform(unsigned char *image, unsigned width, unsigned height, unsigned threadCount) {
+
+}
---
+++ Lab 1/transform.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#include "lodepng.h"
+
+// This method is implemented by rectify.c, pool.c and convolve.c
+void transform(unsigned char *image, unsigned width, unsigned height, unsigned threadCount);
+
+int main(int argc, char *argv[]) {
+    // Check for the command line argument
+    if (argc != 4) {
+        printf("Expected 3 arguments\n");
+        return -1;
+    }
+    // The first argument is the program name, skip it
+    // The next is the input PNG file
+    char *inputName = argv[1];
+    // The next is the output file name
+    char *outputName = argv[2];
+    // The last is the thread count
+    unsigned threadCount = strtoul(argv[3], NULL, 10);
+    if (threadCount <= 0) {
+        // This also occurs if the string is not a number
+        printf("Thread count is 0 or not a number\n");
+        return -1;
+    }
+    // Now load the input PNG file
+    unsigned char *image;
+    unsigned width, height;
+    unsigned readError = lodepng_decode32_file(&image, &width, &height, inputName);
+    if (readError) {
+        printf("Error when loading the input image: %s\n", lodepng_error_text(readError));
+    }
+    // Apply rectification
+    transform(image, width, height, threadCount);
+    // Save the results
+    unsigned outputError = lodepng_encode32_file(outputName, image, width, height);
+    if (outputError) {
+        printf("Error when saving the output image: %s\n", lodepng_error_text(outputError));
+    }
+    // Delete the image
+    free(image);
+    return 0;
+}
