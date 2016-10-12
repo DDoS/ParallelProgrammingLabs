79f63e7bb3c38b0b570bd3ffde33d877d2ee93a9
Aleksi Sapon <aleksi@sapon.ca>
2016-10-12 14:03:43 -0400

    Spaces not tabs and remove trailing whitespace in convolve.c

--- lab1/convolve.c
+++ lab1/convolve.c
@@ -7,59 +7,59 @@
 #define WEIGHTED_MATRIX_HEIGHT  3
 
 void transform(unsigned char **image, unsigned *width, unsigned *height, unsigned threadCount) {
-	unsigned char *imageIn = *image;
+    unsigned char *imageIn = *image;
     unsigned widthIn = *width;
     unsigned heightIn = *height;
-	
-	//Calculate the output width and height
-	unsigned widthOut = widthIn - (WEIGHTED_MATRIX_WIDTH - 1);
+
+    //Calculate the output width and height
+    unsigned widthOut = widthIn - (WEIGHTED_MATRIX_WIDTH - 1);
     unsigned heightOut = heightIn - (WEIGHTED_MATRIX_HEIGHT - 1);
-	
-	//Calculate the number of sub matrix in the main matrix
-	unsigned totalSubMatrices = widthOut * heightOut;
-	
-	//Create output image
-	unsigned char *imageOut = malloc(sizeof(unsigned char) * totalSubMatrices * 4);
+
+    //Calculate the number of sub matrix in the main matrix
+    unsigned totalSubMatrices = widthOut * heightOut;
+
+    //Create output image
+    unsigned char *imageOut = malloc(sizeof(unsigned char) * totalSubMatrices * 4);
     if (imageOut == NULL) {
         printf("Could not allocate new image\n");
         exit(-1);
     }
-	
+
     // Parallelize the for loop
     #pragma omp parallel for num_threads(threadCount)
-	for (unsigned subMatrixIndex = 0; subMatrixIndex < totalSubMatrices; subMatrixIndex++) {
-        
-		unsigned xSubMatrix = subMatrixIndex % widthOut;
+    for (unsigned subMatrixIndex = 0; subMatrixIndex < totalSubMatrices; subMatrixIndex++) {
+
+        unsigned xSubMatrix = subMatrixIndex % widthOut;
         unsigned ySubMatrix = subMatrixIndex / widthOut;
-		
-		unsigned char *currentSubMatrix = imageIn + (xSubMatrix * WEIGHTED_MATRIX_WIDTH +  ySubMatrix * widthIn) * 4;
-		unsigned char addition[3] = {0,0,0};
-		
+
+        unsigned char *currentSubMatrix = imageIn + (xSubMatrix * WEIGHTED_MATRIX_WIDTH +  ySubMatrix * widthIn) * 4;
+        unsigned char addition[3] = {0,0,0};
+
         for (unsigned y = 0; y < WEIGHTED_MATRIX_WIDTH * 4; y+=4) {
-			unsigned char *line = currentSubMatrix + y * widthIn;
+            unsigned char *line = currentSubMatrix + y * widthIn;
             for (unsigned x = 0; x < WEIGHTED_MATRIX_WIDTH * 4; x+=4) {
-				unsigned char *pixel = line + x;
+                unsigned char *pixel = line + x;
                 addition[0] += pixel[0] * w[x/4][y/4];
                 addition[1] += pixel[1] * w[x/4][y/4];
                 addition[2] += pixel[2] * w[x/4][y/4];
-			}
-        }	
-
-        for (int pixelIndex = 0; pixelIndex < 3; pixelIndex++){
-		    if (addition[pixelIndex] <= 0){
-			    addition[pixelIndex] = 0;
-		    }
-		    else if (addition[pixelIndex] >= 255){
-			    addition[pixelIndex] = 255;
             }
         }
-		
+
+        for (int pixelIndex = 0; pixelIndex < 3; pixelIndex++){
+            if (addition[pixelIndex] <= 0){
+                addition[pixelIndex] = 0;
+            }
+            else if (addition[pixelIndex] >= 255){
+                addition[pixelIndex] = 255;
+            }
+        }
+
         unsigned char* pixel = imageOut + subMatrixIndex * 4;
-		pixel[0] = addition[0];
+        pixel[0] = addition[0];
         pixel[1] = addition[1];
         pixel[2] = addition[2];
         pixel[3] = 255;
-		
+
     }
     // Delete the input image
     free(imageIn);
