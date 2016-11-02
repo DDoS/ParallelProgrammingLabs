2c9d7dff90d6343047df671c613abc3bfc653258
Aleksi Sapon <aleksi@sapon.ca>
2016-11-02 12:49:00 -0400

    Implement iteration update

---
+++ lab2/grid.c
@@ -0,0 +1,58 @@
+#include "constant.h"
+#include "node.h"
+
+/*
+    The node at "n" and has coordinates (i, j).
+    It is surrounded, as depicted bellow, by nodes "a", "r", "b" and "l".
+    If the node is not in the middle, 0 is used for non-existing values.
+
+        a
+        |        j
+    l - n - r    ^
+        |        |
+        b        + -- > i
+*/
+void update(unsigned i, unsigned j, Node *n,  Node *a,  Node *r,  Node *b,  Node *l) {
+    if (i == 0) {
+        if (j == 0) {
+            // Corner case
+            n->u = G * r->u;
+            return;
+        }
+        if (j == N - 1) {
+            // Corner case
+            n->u = G * b->u;
+            return;
+        }
+        // Side case
+        n->u = G * r->u;
+        return;
+    }
+    if (i == N - 1) {
+        if (j == 0) {
+            // Corner case
+            n->u = G * l->u;
+            return;
+        }
+        if (j == N - 1) {
+            // Corner case
+            n->u = G * b->u;
+            return;
+        }
+        // Side case
+        n->u = G * l->u;
+        return;
+    }
+    if (j == 0) {
+        // Side case
+        n->u = G * a->u;
+        return;
+    }
+    if (j == N - 1) {
+        // Side case
+        n->u = G * b->u;
+        return;
+    }
+    // Middle case
+    n->u = (RHO * (l->u1 + r->u1 + b->u1 + a->u1 - 4 * n->u1) + 2 * n->u1 - (1 - ETA) * n->u2) / (1 + ETA);
+}
---
+++ lab2/node.h
@@ -0,0 +1,5 @@
+typedef struct {
+    double u;
+    double u1;
+    double u2;
+} Node;
