047243fb3ea2b16e9f2ccee4fa85dcd472d427f9
Aleksi Sapon <aleksi@sapon.ca>
2016-11-04 13:34:26 -0400

    Work on preparing the processes

--- lab2/main.c
+++ lab2/main.c
@@ -1,4 +1,7 @@
+#include <stdlib.h>
 #include <stdio.h>
+
+#include <mpi.h>
 
 #include "grid.h"
 
@@ -15,15 +18,57 @@ void printGrid(Block *block) {
     }
 }
 
+void doProcessWork(Partition *partition, unsigned index, unsigned iterationCount) {
+    // Create the partition block for the process
+    Block block = createBlock(partition, index);
+    // Wait for all processes to be ready for an iteration
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    printf("%d\n", iterationCount);
+
+    //block.nodes[2 + 2 * block.rows].u1 += 1;
+    //updateBlock(&block);
+    //printGrid(&block);
+    //printf("\n");
+}
+
 int main(int argc, char *argv[]) {
-    unsigned processCount = 1;
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
+    // Get the number of processes
+    int processCount;
+    MPI_Comm_size(MPI_COMM_WORLD, &processCount);
+    // Get the rank of the process
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // Create a block partition for the number of processes
     Partition partition = createPartition(processCount);
-    Block block = createBlock(&partition, 0);
-    block.nodes[2 + 2 * block.rows].u1 += 1;
-    updateBlock(&block);
-    printGrid(&block);
-    printf("\n");
-    updateBlock(&block);
-    printGrid(&block);
-    return 0;
+    // Get the iteration count from the arguments and pass it to all processes
+    unsigned iterationCount;
+    signed exitCode = 0;
+    if (rank == 0) {
+        // Check for the command line argument
+        if (argc != 2) {
+            printf("Expected 1 argument\n");
+            exitCode = -1;
+            goto end;
+        }
+        // The first argument is the program name, skip it
+        // The second is the iteration count
+        iterationCount = strtoul(argv[1], NULL, 10);
+        if (iterationCount <= 0) {
+            // This also occurs if the string is not a number
+            printf("Iteration count is 0 or not a number\n");
+            exitCode = -1;
+            goto end;
+        }
+    }
+    // Broadcast the iteration count to all processes
+    MPI_Bcast(&iterationCount, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+    // Do the process work
+    doProcessWork(&partition, rank, iterationCount);
+    // Finalize the MPI environment
+    end:
+    MPI_Finalize();
+    return exitCode;
 }
