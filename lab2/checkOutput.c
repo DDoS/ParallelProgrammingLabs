b4807d7aaf19be2e662391b6f1a2854800c43c5c
Aleksi Sapon <aleksi@sapon.ca>
2016-11-11 13:41:47 -0500

    Fix checkOutput.c and add it to the makefile

--- lab2/checkOutput.c
+++ lab2/checkOutput.c
@@ -1,17 +1,14 @@
+#include <stdio.h>
+
 #include "output.h"
 #include "outputProg.h"
 
-int main(char argc, char* argv){
+int main(int argc, char *argv[]) {
   float cumSum = 0;
-
-  int i=0;
-  for (i=0 ; i<2000 ; i++){
-    cumSum += (output[i]-outputProg[i])*(output[i]-outputProg[i]);
-    printf("%f\n",cumSum);
+  for (int i = 0; i < 2000; i++) {
+    cumSum += (output[i] - outputProg[i]) * (output[i] - outputProg[i]);
   }
-  float result = cumSum/2000;
-
-  printf("\n\n\n%f\n", result);
-
+  float result = cumSum / 2000;
+  printf("Mean squared error: %f\n", result);
   return 0;
 }
