5dc108cbba9d9549759bb85aab1db90d60b7d261
Aleksi Sapon <aleksi@sapon.ca>
2016-11-19 15:40:32 -0500

    Implement rectify

---
+++ lab3/rectify.cu
@@ -0,0 +1,20 @@
+#include "transform.h"
+
+__global__ void transform(unsigned char* output, cudaTextureObject_t texture, int width, int height) {
+    // Calculate texture coordinates
+    unsigned x = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned y = blockIdx.y * blockDim.y + threadIdx.y;
+    // Check that we are within bounds
+    if (x >= width || y >= height) {
+        return;
+    }
+    // Read the pixel data from the texture
+    int4 pixel = tex2D<int4>(texture, x, y);
+    // Calculate the output pixel address
+    unsigned char* outputPixelAddress = output + (y * width + x) * 4;
+    // Set the rectified pixel components (ignoring the alpha component)
+    outputPixelAddress[0] = max(pixel.x, 127);
+    outputPixelAddress[1] = max(pixel.y, 127);
+    outputPixelAddress[2] = max(pixel.z, 127);
+    outputPixelAddress[3] = pixel.w;
+}
